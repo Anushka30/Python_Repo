bfe77a5b56ba3b3088d306238b29a9b1df5097f5
Anushka30 <anushka30j@gmail.com>
2019-10-10 20:08:49 +0530

    Added changes

--- Deep_Copy.py
+++ Deep_Copy.py
@@ -3,6 +3,7 @@ When a deep copy in Python creates a new object, it inserts into the new object 
 object. In other words, it copies an object into another. This means any changes we make to the copy do not reflect
 in the original.
 """
+
 import copy
 
 list1 = [1, 2, 3, 4, 5]
@@ -12,5 +13,5 @@ list2 = copy.deepcopy(list1)
 list1.append(11)
 list1[0] = 9
 list2.append(10)
-print(list2)
-print(list1)
+print("List2: ", list2)
+print("List1: ",list1)
